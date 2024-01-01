9edb3c2de43e82ad6b1b5686a7274cf216e12e4d
Nikolaos Perperidis <Nikolaos.Perperidis@fenergo.com>
2024-01-01 20:15:24 +0200

    check

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
     return <div>
         <h1>Home</h1>
         <button onClick={() => navigate('/login')}>
-            go to login
+            login 
         </button>
         <button onClick={() => navigate('/signup')}>
             go to signup
